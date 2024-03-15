60b2bd8462612d67260efe4fe375aecbed64a66f
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2024-03-15 23:27:07 +0800

    [Nicolo] Added SignUpAs, SignUp and Verification Activities

---
+++ app/src/main/java/com/example/roadready/SignUpAs_Activity.java
@@ -0,0 +1,14 @@
+package com.example.roadready;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpAs_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_as);
+    }
+}
